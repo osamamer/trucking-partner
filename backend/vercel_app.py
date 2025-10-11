b9e3b8952c58f9edfa9870d133b2fc5dc0079930
osama <commercialcomedy02@gmail.com>
2025-10-11 19:37:49 +0300

    - vercel_app.py

--- backend/vercel_app.py
+++ backend/vercel_app.py
@@ -1,5 +1,7 @@
+import sys
+
 from django.core.wsgi import get_wsgi_application
 import os
-
+sys.path.insert(0, os.path.join(os.path.dirname(__file__)))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 application = get_wsgi_application()
